843773512c792e47c26aa552b95532681fede311
rickyall <783814127@qq.com>
2016-10-22 11:22:39 +0800

    init

---
+++ spec/stock-spec.js
@@ -0,0 +1,73 @@
+'use babel';
+
+import Stock from '../lib/stock';
+
+// Use the command `window:run-package-specs` (cmd-alt-ctrl-p) to run specs.
+//
+// To run a specific `it` or `describe` block add an `f` to the front (e.g. `fit`
+// or `fdescribe`). Remove the `f` to unfocus the block.
+
+describe('Stock', () => {
+  let workspaceElement, activationPromise;
+
+  beforeEach(() => {
+    workspaceElement = atom.views.getView(atom.workspace);
+    activationPromise = atom.packages.activatePackage('stock');
+  });
+
+  describe('when the stock:toggle event is triggered', () => {
+    it('hides and shows the modal panel', () => {
+      // Before the activation event the view is not on the DOM, and no panel
+      // has been created
+      expect(workspaceElement.querySelector('.stock')).not.toExist();
+
+      // This is an activation event, triggering it will cause the package to be
+      // activated.
+      atom.commands.dispatch(workspaceElement, 'stock:toggle');
+
+      waitsForPromise(() => {
+        return activationPromise;
+      });
+
+      runs(() => {
+        expect(workspaceElement.querySelector('.stock')).toExist();
+
+        let stockElement = workspaceElement.querySelector('.stock');
+        expect(stockElement).toExist();
+
+        let stockPanel = atom.workspace.panelForItem(stockElement);
+        expect(stockPanel.isVisible()).toBe(true);
+        atom.commands.dispatch(workspaceElement, 'stock:toggle');
+        expect(stockPanel.isVisible()).toBe(false);
+      });
+    });
+
+    it('hides and shows the view', () => {
+      // This test shows you an integration test testing at the view level.
+
+      // Attaching the workspaceElement to the DOM is required to allow the
+      // `toBeVisible()` matchers to work. Anything testing visibility or focus
+      // requires that the workspaceElement is on the DOM. Tests that attach the
+      // workspaceElement to the DOM are generally slower than those off DOM.
+      jasmine.attachToDOM(workspaceElement);
+
+      expect(workspaceElement.querySelector('.stock')).not.toExist();
+
+      // This is an activation event, triggering it causes the package to be
+      // activated.
+      atom.commands.dispatch(workspaceElement, 'stock:toggle');
+
+      waitsForPromise(() => {
+        return activationPromise;
+      });
+
+      runs(() => {
+        // Now we can test for view visibility
+        let stockElement = workspaceElement.querySelector('.stock');
+        expect(stockElement).toBeVisible();
+        atom.commands.dispatch(workspaceElement, 'stock:toggle');
+        expect(stockElement).not.toBeVisible();
+      });
+    });
+  });
+});
---
+++ spec/stock-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import StockView from '../lib/stock-view';
+
+describe('StockView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('life');
+  });
+});
